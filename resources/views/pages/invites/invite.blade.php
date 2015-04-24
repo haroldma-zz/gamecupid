fe67e6c5691803d2bc4ccc2d4a7537c28baa8f6f
Sharif <s.paksa@icloud.com>
2015-04-24 15:18:22 +0200

    Some more work on the invite form

--- resources/views/pages/invites/invite.blade.php
+++ resources/views/pages/invites/invite.blade.php
@@ -5,27 +5,37 @@
 	<div class="row">
 		<div class="medium-12 columns">
 			<div class="panel extra">
-				<h2 class="light-header">
-					Invite
-				</h2>
-				<hr>
 				<div class="row">
 					<div class="medium-8 medium-offset-2 columns">
+						<h2 class="light-header">
+							Invite
+						</h3>
+						<hr>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, a, ea voluptate nemo asperiores cum ullam commodi neque voluptatem! Aut beatae delectus neque, assumenda ratione, odit possimus mollitia eum, ipsum dolor error.
+						</p>
+						<br>
 						{!! Form::open(['url' => '/invite', 'class' => 'form']) !!}
-						{!! Form::label('game', 'I want to submit an invite for...') !!}
-						{!! Form::select('game', [
+						{!! Form::label('game_id', 'I want to submit an invite for...') !!}
+						{!! Form::select('game_id', [
 												'0' => 'Select a game'
 												 ],
 												0,
 												['class' => 'form-control']) !!}
-						{!! Form::label('console', 'on the...') !!}
-						{!! Form::select('console', ['0' => 'Select a console',
+						{!! Form::label('console_id', 'on the...') !!}
+						{!! Form::select('console_id', ['0' => 'Select a console',
 													 '1' => 'Xbox 360',
 													 '2' => 'Xbox One',
 													 '3' => 'Playstation 3',
 													 '4' => 'Playstation 4',
 													 '5' => 'PC'],
 													0,['class' => 'form-control']) !!}
+						{!! Form::label('title', 'Title') !!}
+						{!! Form::text('title', '', ['form-control']) !!}
+						{!! Form::label('self_text', 'Description') !!}
+						{!! Form::textarea('self_text', '', ['form-control']) !!}
+						{!! Form::checkbox('requires_approval', 1) !!}
+						{!! Form::label('requires_approval', 'When someone wants to play with me, that person requires my approval.') !!}
 
 						<br><br><br>
 						{!! Recaptcha::render() !!}
