2e2ce9003003992045b880fc7b9e276a471789be
Sharif <s.paksa@icloud.com>
2015-05-05 14:42:12 +0200

    Fixed posts migration

--- database/migrations/2015_04_23_084656_create_posts_table.php
+++ database/migrations/2015_04_23_084656_create_posts_table.php
@@ -15,19 +15,19 @@ class CreatePostsTable extends Migration {
 		Schema::create('posts', function(Blueprint $table) {
             $table->increments('id');
             $table->tinyInteger('type');
+            $table->tinyInteger('is_self_text');
+            $table->tinyInteger('category');
             $table->string('title');
-            $table->mediumText('self_text');
-            $table->mediumText('markdown_text');
-            $table->string('tag_text');
             $table->string('slug');
+            $table->mediumText('markdown_text');
+            $table->mediumText('self_text');
+            $table->string('flair_text');
             $table->integer('max_players');
             $table->tinyInteger('featured');
             $table->tinyInteger('verified_only');
             $table->integer('user_id');
             $table->integer('game_id');
             $table->integer('console_id');
-            $table->integer('crew_id');
-
             $table->timestamps();
         });
 	}
