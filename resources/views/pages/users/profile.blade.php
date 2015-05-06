1a84b20a6d977f37aaa323e689671d3788987f02
Sharif <s.paksa@icloud.com>
2015-05-06 16:10:36 +0200

    Improved Settings & Profile pages

--- resources/views/pages/users/profile.blade.php
+++ resources/views/pages/users/profile.blade.php
@@ -2,15 +2,38 @@
 
 @section('page')
 <section class="page">
-	<div class="row">
-		<div class="medium-9 columns">
-			<div class="panel">
-				<h1>PROFILE OF {{ $user->username }}</h1>
+	<div class="row medium-collapse detailpage">
+		<div class="medium-8 medium-offset-2 columns">
+			<div class="panel extra text-center">
+				<h4>
+					{{ $user->username }}'s profile
+				</h4>
+				<header>
+					<div class="profile-picture"></div>
+				</header>
+				<section>
+					<ul class="no-bullet">
+						<li><a href="">Add as friend</a></li>
+						<li><a href="">Send message</a></li>
+					</ul>
+				</section>
+				<hr>
+				<footer>
+					<h5 class="super-header">Achievements</h5>
+					<br>
+					@if ($user->profiles()->count() > 0)
+						@foreach($user->profiles as $profile)
+						<img src="{{ url('img/badges/' . $profile->platform->shortname) . '.png' }}">
+						@endforeach
+					@else
+					<p>
+						{{ $user->username }} has no badges yet.
+					</p>
+					@endif
+				</footer>
 			</div>
-		</div>
-		<div class="medium-3 columns">
-			<div class="panel">
-				<h5 class="super-header">Achievements</h5>
+			<div class="panel extra">
+				<h5 class="super-header">Activity</h5>
 			</div>
 		</div>
 	</div>
--- resources/views/pages/users/settings.blade.php
+++ resources/views/pages/users/settings.blade.php
@@ -3,47 +3,10 @@
 @section('page')
 <section class="page">
 	<div class="row">
-		<div class="medium-12 columns">
-			<div class="panel">
-				<div class="row">
-					<div class="medium-6 columns">
-						<h4>
-							Connected profiles ({{ Auth::user()->profiles()->count() }})
-						</h4>
-						<hr>
-	                    @if (Session::has('notice'))
-	                        @if (Session::get('notice')[0] == 'error')
-	                            <ul class="no-bullet text-alert smaller-fs">
-	                        @elseif (Session::get('notice')[0] == 'info')
-	                            <ul class="no-bullet text-info smaller-fs">
-	                        @elseif (Session::get('notice')[0] == 'success')
-	                        	<ul class="no-bullet text-success smaller-fs">
-	                        @endif
-	                        <li>{{ Session::get('notice')[1] }}</li>
-	                        </ul>
-	                    @endif
-						@if(Auth::user()->profiles()->count() === 0)
-						<p>
-							You don't have any online gaming profiles connected to this account.
-							<br><br><br>
-							Connect your online gaming profiles by clicking on a platform under 'Connect'.
-						</p>
-						@else
-                            @foreach (Auth::user()->profiles as $profile)
-								<div class="profile">
-		                            <img src="{{ $profile->platform->logo_url  }}" />
-		                            <ul class="inline-list profiles-mini-list">
-		                            	<li>{{ $profile->online_username }}</li>
-		                            	<li><a href="/account/disconnect/{{ $profile->platform->shortname }}/{{ $profile->online_username }}" id="openDialog"
-		                            		data-type="confirm"
-		                            		data-message="Are you sure you want to disconnect {{ $profile->online_username }}?"
-		                            		>disconnect</a></li>
-		                            </ul>
-								</div>
-                            @endforeach
-						@endif
-					</div>
-					<div class="medium-3 columns">
+		<div class="medium-6 medium-offset-3 columns">
+			<div class="row">
+				<div class="medium-6 columns">
+					<div class="panel">
 						<h4>
 							Connect
 						</h4>
@@ -56,13 +19,63 @@
 							</ul>
 						</p>
 					</div>
-					<div class="medium-3 columns">
+				</div>
+				<div class="medium-6 columns">
+					<div class="panel">
 						<h4>
-							Privacy
+							Account
 						</h4>
 						<hr>
+						<p>
+							<ul class="no-bullet">
+								<li><a href="">Edit profile</a></li>
+								<li><a href="">Change timezone</a></li>
+								<li><a href="">Change password</a></li>
+							</ul>
+						</p>
 					</div>
 				</div>
+			</div>
+			<div class="panel">
+				<h4>
+					Connected network profiles ({{ Auth::user()->profiles()->count() }})
+				</h4>
+				<hr>
+                @if (Session::has('notice'))
+                    @if (Session::get('notice')[0] == 'error')
+                        <ul class="no-bullet text-alert smaller-fs">
+                    @elseif (Session::get('notice')[0] == 'info')
+                        <ul class="no-bullet text-info smaller-fs">
+                    @elseif (Session::get('notice')[0] == 'success')
+                    	<ul class="no-bullet text-success smaller-fs">
+                    @endif
+                    <li>{{ Session::get('notice')[1] }}</li>
+                    </ul>
+                @endif
+				@if(Auth::user()->profiles()->count() === 0)
+				<p>
+					You don't have any online gaming profiles connected to this account.
+					<br><br><br>
+					Connect your online gaming profiles by clicking on a platform under 'Connect'.
+				</p>
+				@else
+				<div class="row">
+                    @foreach (Auth::user()->profiles as $profile)
+                    <div class="medium-4 columns end">
+						<div class="profile">
+                            <img src="{{ $profile->platform->logo_url  }}" />
+                            <ul class="inline-list profiles-mini-list">
+                            	<li>{{ $profile->online_username }}</li>
+                            	<li><a href="/account/disconnect/{{ $profile->platform->shortname }}/{{ $profile->online_username }}" id="openDialog"
+                            		data-type="confirm"
+                            		data-message="Are you sure you want to disconnect {{ $profile->online_username }}?"
+                            		>disconnect</a></li>
+                            </ul>
+						</div>
+					</div>
+                    @endforeach
+				</div>
+				@endif
 			</div>
 		</div>
 	</div>
