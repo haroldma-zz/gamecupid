007e2ae13938667e2ae2782fa877042b9a4ce521
Sharif <s.paksa@icloud.com>
2015-05-21 18:07:27 +0200

    Added PHPDocs to AvailableConsole & Comment models.

--- app/Models/AvailableConsole.php
+++ app/Models/AvailableConsole.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Database\Eloquent\Model;
 
-class AvailableConsole extends Model {
+/**
+* AvailableConsole model
+*
+* This model is used to check for which consoles
+* a game is available.
+*
+* @uses     Illuminate\Database\Eloquent\Model
+* @category Models
+*/
+class AvailableConsole extends Model
+{
 
 	/**
 	 * The database table used by the model.
@@ -12,11 +22,12 @@ class AvailableConsole extends Model {
 	protected $table = 'available_console';
 
 
-	/**
-	*
-	* Relations
-	*
-	**/
+    /**
+     * Get the console instance associated with the game.
+     *
+     * @uses  	App\Models\Console
+     * @return 	Console
+     */
 	public function console()
 	{
 		return $this->hasOne('App\Models\Console', 'id', 'console_id');
